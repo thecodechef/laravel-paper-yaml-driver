d3b00692a1e1c035a5776277339f1c225002a11b
Jeremy Bolding <cyberchefjay@gmail.com>
2026-08-20 13:43:19 -0500

    chore: switch some stuff around

--- src/YamlDriver.php
+++ src/YamlDriver.php
@@ -22,7 +22,7 @@ final readonly class YamlDriver implements DriverContract
         if ($content === false) {
             throw FileParseException::unreadable($filepath);
         }
-        $data = Yaml::dump($content, 2, 2);
+        $data = Yaml::parse($content);
 
         return is_array($data) ? $data : [];
     }
@@ -31,6 +31,6 @@ final readonly class YamlDriver implements DriverContract
     {
         unset($data['slug']);
 
-        return Yaml::parse($data);
+        return Yaml::dump($data, 2, 2);
     }
 }
