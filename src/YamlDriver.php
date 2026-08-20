a6285ad35e52dd9206c29d89c12c3ec5c3c24257
Jeremy Bolding <cyberchefjay@gmail.com>
2026-08-20 12:42:57 -0500

    fixed: `Symfony\Component\Yaml\Yaml` typo to `Symfony\Component\Yaml`

--- src/YamlDriver.php
+++ src/YamlDriver.php
@@ -6,7 +6,7 @@ namespace LaravelPaperYamlDriver\YamlDriver;
 
 use JacobJoerjensen\LaravelPaper\Contracts\DriverContract;
 use JacobJoerjensen\LaravelPaper\Exceptions\FileParseException;
-Symfony\Component\Yaml;
+use Symfony\Component\Yaml;
 
 final readonly class YamlDriver implements DriverContract
 {
