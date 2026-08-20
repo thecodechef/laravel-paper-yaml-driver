e2cebaa6a982a7e59658081c2106cc51408cb10c
Jeremy Bolding <cyberchefjay@gmail.com>
2026-08-20 10:03:41 -0500

    fixed: some return type errors.

--- src/YamlDriverServiceProvider.php
+++ src/YamlDriverServiceProvider.php
@@ -24,11 +24,6 @@ class YamlDriverServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/laravel-paper-yaml-driver.php');
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-paper-yaml-driver');
-
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'laravel-paper-yaml-driver');
 
         if (! $this->app->runningInConsole()) {
             return;
@@ -37,22 +32,6 @@ class YamlDriverServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/laravel-paper-yaml-driver.php' => config_path('laravel-paper-yaml-driver.php'),
         ], ['laravel-paper-yaml-driver', 'laravel-paper-yaml-driver-config']);
-
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-paper-yaml-driver'),
-        ], ['laravel-paper-yaml-driver', 'laravel-paper-yaml-driver-views']);
-
-        $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath('vendor/laravel-paper-yaml-driver'),
-        ], ['laravel-paper-yaml-driver', 'laravel-paper-yaml-driver-lang']);
-
-        $this->publishes([
-            __DIR__.'/../public' => public_path('vendor/laravel-paper-yaml-driver'),
-        ], ['laravel-paper-yaml-driver', 'laravel-paper-yaml-driver-assets']);
-
-        $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ], ['laravel-paper-yaml-driver', 'laravel-paper-yaml-driver-migrations']);
 
         $this->commands([
             YamlDriverCommand::class,
